8c577a18678a1f0c418c869ca4f6b39f3c205cb1
lhj9520 <gw9520@naver.com>
2022-12-06 23:05:01 +0900

    221206 회원가입 기능 구현

---
+++ server/server.js
@@ -0,0 +1,216 @@
+const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
+const mysql = require("mysql2");
+const md5 = require("md5");
+
+const db = mysql.createPoolCluster();
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+app.use(
+  session({
+    secret: "SECRET",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+db.add("project", {
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  database: "frontend_project",
+  port: 3306,
+});
+
+function runDB(query) {
+  return new Promise(function (resolve, reject) {
+    db.getConnection("project", function (error, connection) {
+      if (error) {
+        console.log("DB Connection Error!", error);
+        reject(true);
+      }
+
+      connection.query(query, function (error, data) {
+        if (error) {
+          console.log("query error", error);
+          reject(true);
+        }
+
+        resolve(data);
+      });
+      connection.release();
+    });
+  });
+}
+
+app.get("/", (req, res) => {
+  res.send("여기로 옵니다!");
+});
+
+app.post("/idcheck", async (req, res) => {
+  /**
+   * 아이디 중복체크
+   */
+  // console.log(req.body);
+  const id = req.body.id;
+
+  const result = {
+    code: "success",
+    message: "사용 가능한 아이디입니다.",
+  };
+
+  const queryresult = await runDB(
+    `SELECT * FROM user WHERE mem_userid = '${id}'`
+  );
+
+  if (queryresult.length > 0) {
+    result.code = "error";
+    result.message = "이미 사용중인 아이디입니다.";
+    res.send(result);
+    return;
+  }
+
+  res.send(result);
+});
+
+app.post("/emailcheck", async (req, res) => {
+  /**
+   * 이메일 중복체크
+   */
+  // console.log(req.body);
+  const email = req.body.email;
+
+  const result = {
+    code: "success",
+    message: "사용 가능한 이메일입니다.",
+  };
+
+  const queryresult = await runDB(
+    `SELECT * FROM user WHERE mem_email = '${email}'`
+  );
+
+  if (queryresult.length > 0) {
+    result.code = "error";
+    result.message = "이미 사용중인 이메일입니다.";
+    res.send(result);
+    return;
+  }
+
+  res.send(result);
+});
+
+app.post("/nicknamecheck", async (req, res) => {
+  /**
+   * 닉네임 중복체크
+   */
+  console.log(req.body);
+  const nickname = req.body.nickname;
+
+  const result = {
+    code: "success",
+    message: "사용 가능한 닉네임입니다.",
+  };
+
+  const queryresult = await runDB(
+    `SELECT * FROM user WHERE mem_nickname = '${nickname}'`
+  );
+
+  if (queryresult.length > 0) {
+    result.code = "error";
+    result.message = "이미 사용중인 닉네임입니다.";
+    res.send(result);
+    return;
+  }
+
+  res.send(result);
+});
+
+app.post("/join", async (req, res) => {
+  /**
+   *
+   * DB에 id,nickname,pw insert
+   */
+  const { id, pw, name, birth, email, gender, nickname, phone } = req.body;
+
+  const hashpw = md5(pw);
+  const month = birth.month.length === 1 ? "0" + birth.month : birth.month;
+  const day = birth.day.length === 1 ? "0" + birth.day : birth.day;
+  const bdate = `${birth.year}-${month}-${day}`;
+
+  const result = {
+    code: "success",
+    message: "회원가입이 완료되었습니다!",
+  };
+
+  let queryresult = "";
+  phone === ""
+    ? (queryresult = `INSERT INTO USER(mem_userid,mem_password,mem_username,mem_birth,mem_email,mem_gender,mem_nickname,mem_phone,mem_regtime) VALUES ('${id}','${hashpw}','${name}','${bdate}','${email}','${gender}','${nickname}',NULL,NOW())`)
+    : (queryresult = `INSERT INTO USER(mem_userid,mem_password,mem_username,mem_birth,mem_email,mem_gender,mem_nickname,mem_phone,mem_regtime) VALUES ('${id}','${hashpw}','${name}','${bdate}','${email}','${gender}','${nickname}','${phone}',NOW())`);
+  await runDB(queryresult);
+
+  res.send(result);
+});
+
+app.post("/login", async (req, res) => {
+  /**
+   * 디비에서 아이디&비번 확인
+   */
+  console.log(req.body);
+  const id = req.body.id;
+  const pw = req.body.pw;
+  const autologin = req.body.autologin;
+
+  const result = {
+    code: "success",
+    message: "로그인 성공",
+  };
+
+  //비밀번호 hash 암호화 MD5로
+  const hashpw = md5(pw);
+  console.log(hashpw);
+
+  const queryresult = await runDB(
+    `SELECT * FROM user WHERE mem_userid = '${id}' and mem_password = '${hashpw}'`
+  );
+
+  if (queryresult.length === 0) {
+    result.code = "error";
+    result.message = "로그인에 실패하였습니다!";
+    res.send(result);
+    return;
+  }
+
+  /**
+   * 로그인 세션 회원정보 저장
+   */
+  req.session.loginUser = queryresult[0];
+  req.session.save();
+
+  res.send(result);
+});
+
+app.get("/user", (req, res) => {
+  console.log(req.session.loginUser);
+  res.send(req.session.loginUser);
+});
+
+app.get("/userdelete", (req, res) => {
+  req.session.loginUser = {};
+  req.session.save();
+  res.send(req.session.loginUser);
+});
+
+app.listen(port, () => {
+  console.log("서버가 시작되었습니다");
+});
