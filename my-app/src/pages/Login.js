a26bc17b35122e34b96e1aef1ffbdda3c339e0d6
lhj9520 <gw9520@naver.com>
2023-02-07 20:01:16 +0900

    로그인 login.js useSate 수정

--- my-app/src/pages/Login.js
+++ my-app/src/pages/Login.js
@@ -10,50 +10,54 @@ import { useNavigate } from "react-router-dom";
 function Login() {
   const navigation = useNavigate();
 
-  const [idstatus, setIdtatus] = React.useState(true);
-  const [pwstatus, setPwtatus] = React.useState(true);
-  const [id, setId] = React.useState("");
-  const [pw, setPw] = React.useState("");
-  const [emsg, setEmsg] = React.useState("");
-  const [autologin, Setautologin] = React.useState(false);
+  const [userlogin, setUserlogin] = React.useState({
+    id: "",
+    pw: "",
+    msg1: "",
+    msg2: "",
+    autologin: false,
+  });
 
   const idchange = (event) => {
-    const vid = event.target.value;
-    setId(vid);
-    //아이디 예외처리
-    if (vid === "") {
-      setIdtatus(false);
-    } else {
-      setIdtatus(true);
+    const data = event.target.value;
+    if (data.length <= 12) {
+      setUserlogin((prevState) => {
+        return { ...prevState, id: data, msg1: "" };
+      });
     }
   };
 
   const pwchange = (event) => {
-    const vpw = event.target.value;
-    setPw(vpw);
-    //비밀번호 길이 예외처리
-    if (vpw === "") {
-      setEmsg("비밀번호를 입력해주세요.");
-      setPwtatus(false);
-    } else if (pw.length > 19 || pw.length < 7) {
-      setEmsg("8~20자까지 입력가능합니다.");
-      setPwtatus(false);
-    } else {
-      setEmsg("");
-      setPwtatus(true);
+    const data = event.target.value;
+    if (data.length <= 20) {
+      setUserlogin((prevState) => {
+        return { ...prevState, pw: data, msg2: "" };
+      });
     }
   };
 
-  const userlogin = async () => {
-    //코드 확인
-    if (!idstatus || !pwstatus) {
+  const userloginHandler = async () => {
+    if (!userlogin.id) {
+      setUserlogin((prevState) => {
+        return { ...prevState, msg1: "아이디를 입력하세요.", msg2: "" };
+      });
+      return;
+    }
+    if (!userlogin.pw) {
+      setUserlogin((prevState) => {
+        return { ...prevState, msg2: "비밀번호를 입력하세요.", msg1: "" };
+      });
       return;
     }
     //자동로그인 확인
     await axios({
       url: "http://localhost:5000/login",
       method: "POST",
-      data: { id: id, pw: pw, autologin: autologin },
+      data: {
+        id: userlogin.id,
+        pw: userlogin.pw,
+        autologin: userlogin.autologin,
+      },
     })
       .then((res) => {
         const { code, message } = res.data;
@@ -73,13 +77,15 @@ function Login() {
   //체크 확인(자동로그인)
   const handleClickRadioButton = (e) => {
     if (e.target.checked) {
-      //체크
       // console.log("체크");
-      Setautologin(true);
+      setUserlogin((prevState) => {
+        return { ...prevState, autologin: true };
+      });
     } else {
-      //안체크
       // console.log("안체크");
-      Setautologin(false);
+      setUserlogin((prevState) => {
+        return { ...prevState, autologin: false };
+      });
     }
   };
 
@@ -97,27 +103,29 @@ function Login() {
           <span className="title">로그인</span>
           <input
             type="text"
+            maxLength={12}
+            value={userlogin.id}
             name="id"
             placeholder="아이디"
             className="item"
             onChange={idchange}
           />
-          <span className={classnames("msg", { close: idstatus })}>
-            아이디를 입력해주세요.
-          </span>
+          <span className="msg">{userlogin.msg1}</span>
           <input
             type="password"
+            maxLength={20}
+            value={userlogin.pw}
             name="pw"
             placeholder="비밀번호"
             className="item pwbox"
             onChange={pwchange}
           />
-          <span className={classnames("msg", { close: pwstatus })}>{emsg}</span>
+          <span className="msg">{userlogin.msg2}</span>
           {/* <span className="autologincheck">
             <input type="checkbox" onClick={handleClickRadioButton} />
             자동로그인
           </span> */}
-          <button className="loginbtn" onClick={userlogin}>
+          <button className="loginbtn" onClick={userloginHandler}>
             로그인
           </button>
         </div>
