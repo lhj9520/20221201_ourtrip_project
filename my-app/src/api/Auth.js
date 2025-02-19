8bdda62655ec4b16b601c2c8e96833f84739d01c
lhj9520 <lhj9520@gmail.com>
2025-02-19 19:10:08 +0900

    수정

--- my-app/src/api/Auth.js
+++ my-app/src/api/Auth.js
@@ -30,6 +30,7 @@ export const getLogin = async (user_id, user_pw) => {
 export const getLoginSession = async () => {
   try {
     const response = await axios(`${BASE_URL}/auth/authcheck`);
+
     return response.data;
   } catch (error) {
     console.error("로그인 세션 가져오기 실패:", error);
